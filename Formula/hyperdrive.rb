7d85e91a1c359feb0e43cf00eaa0ec075265eedd
Alec Reynolds <alec+git@lando.dev>
2022-10-05 11:24:26 -0700

    Feed the linter with formatting fixes.

--- Formula/hyperdrive.rb
+++ Formula/hyperdrive.rb
@@ -1,9 +1,9 @@
 class Hyperdrive < Formula
+  desc "First-class package and dependency manager for Lando"
   arch = Hardware::CPU.arm? ? "arm64" : "amd64"
-  version "v0.7.0-alpha.12"
-  desc "A first-class package and dependency manager for Lando."
+  version "0.7.0-alpha.12"
   homepage "https://github.com/lando/hyperdrive"
-  url "https://github.com/lando/hyperdrive/releases/download/#{version}/hyperdrive-macos-#{arch}-#{version}.tar.gz"
+  url "https://github.com/lando/hyperdrive/releases/download/v#{version}/hyperdrive-macos-#{arch}-v#{version}.tar.gz"
   sha256 "fa86e5962e6ece4b5e367675293eec91d52aa2f2f8fa8ae4a3a7bcf49b7451a1"
   license "Apache"
 
@@ -11,7 +11,7 @@ class Hyperdrive < Formula
 
   def install
     arch = Hardware::CPU.arm? ? "arm64" : "amd64"
-    bin.install "hyperdrive-macos-#{arch}-#{version}" => "hyperdrive"
+    bin.install "hyperdrive-macos-#{arch}-v#{version}" => "hyperdrive"
   end
 
   on_linux do
