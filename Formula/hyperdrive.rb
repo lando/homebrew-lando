b968d8ddbde853a7c51e15149b75b0217ff31346
Alec Reynolds <alec+git@lando.dev>
2022-10-05 11:39:35 -0700

    Get rid of tap-syntax check, but update to match as closely as possible.

--- Formula/hyperdrive.rb
+++ Formula/hyperdrive.rb
@@ -1,11 +1,11 @@
 class Hyperdrive < Formula
   desc "First-class package and dependency manager for Lando"
   arch = Hardware::CPU.arm? ? "arm64" : "amd64"
-  version "0.7.0-alpha.12"
   homepage "https://github.com/lando/hyperdrive"
+  version "0.7.0-alpha.12"
   url "https://github.com/lando/hyperdrive/releases/download/v#{version}/hyperdrive-macos-#{arch}-v#{version}.tar.gz"
   sha256 "fa86e5962e6ece4b5e367675293eec91d52aa2f2f8fa8ae4a3a7bcf49b7451a1"
-  license "Apache"
+  license "AGPL-3.0-only"
 
   uses_from_macos "curl"
 
